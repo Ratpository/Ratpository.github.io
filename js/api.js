a36bd7c3267f4118b1747861d84696ce99f9dc7b
Ratpository <125371999+Ratpository@users.noreply.github.com>
2023-08-14 11:03:57 -0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,13 +1,49 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
-const port = process.env.PORT || 3001;
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use(express.json());
+// Define API routes
 
-// Your cart management and Stripe payment routes here
+// Endpoint to add an item to the cart
+app.post('/api/add-to-cart', (req, res) => {
+  const { itemId, quantity } = req.body;
+  // Add item to the cart and send a response
+  // Replace this with your actual cart logic
+  res.json({ message: 'Item added to cart successfully' });
+});
 
+// Endpoint to process checkout and payment
+app.post('/api/checkout', async (req, res) => {
+  try {
+    const { items, currency } = req.body;
+
+    // Create a paymentIntent using Stripe
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateTotalAmount(items), // Calculate the total amount based on items
+      currency,
+    });
+
+    // Send the paymentIntent client secret to the client
+    res.json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while processing the payment.' });
+  }
+});
+
+// Calculate the total amount based on items
+function calculateTotalAmount(items) {
+  // Replace this with your actual calculation logic
+  return 1000; // Example total amount in cents
+}
+
+// Start the server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
